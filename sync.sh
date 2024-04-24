448946d718e664142404941a879555bcd0bf519b
daisuke718 <daisuke718@users.noreply.github.com>
2024-04-24 19:58:42 +0900

    add delete branch

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-current_branch=$(git branch -vv | grep "\[" | grep "\*" | awk '{print $2}')
+current_branch=$(git branch -vv | grep "\[" | grep -v " gone" | grep "\*" | awk '{print $2}')
 if [ -n "$current_branch" ] ; then
   behind_commit=$(git rev-list --right-only --count "$current_branch"...origin/"$current_branch") 
   if [ "$behind_commit" -gt 0 ] ; then
@@ -10,9 +10,14 @@ fi
 
 for branch in $(git branch -vv | grep "\[" | grep -v "\*" | awk '{print $1}')
 do
-  behind_commit=$(git rev-list --right-only --count "$branch"...origin/"$branch")
-  if [ "$behind_commit" -gt 0 ] ; then
-    git fetch origin "$branch":"$branch"
+  gone_remote=$(git branch -vv | grep " $branch .* gone")
+  if [ -n "$gone_remote" ] ; then
+    git branch -D $branch
+  else 
+    behind_commit=$(git rev-list --right-only --count "$branch"...origin/"$branch")
+    if [ "$behind_commit" -gt 0 ] ; then
+      git fetch origin "$branch":"$branch"
+    fi
   fi
 done
 
