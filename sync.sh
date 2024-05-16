d0d49c1df2081dbcdce33adf535fcead19d61d18
daisuke718 <daisuke718@users.noreply.github.com>
2024-05-16 13:16:37 +0900

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,20 +3,19 @@
 git prune
 
 current_branch=$(git symbolic-ref -q --short HEAD)
-git for-each-ref --format='%(refname:short) %(upstream:short)' refs/heads | while read branch upstream
+git for-each-ref --format='%(refname:short) %(upstream:short) $(upstream:track)' refs/heads | while read branch upstream
 do
   if [ -n "$upstream" ]; then
-    behind_commit=$(git rev-list --right-only --count "$current_branch"...origin/"$current_branch") 
-    if [ "$behind_commit" -gt 0 ]; then
-      if [ "$branch" = "$current_branch" ]; then
-        git pull
-      else
-        git fetch "$branch":"$upstream"
-      fi
-    fi
-  else
-    if [ "$branch" != "$current_branch" ]; then
+    if [ "$track" = "[gone]" ]; then
       git branch -D $branch
+    else
+      if [[ "$track" =~ "behind" ]]; then
+        if [ "$branch" = "$current_branch" ]; then
+          git pull
+        else
+          git fetch "$branch":"$upstream"
+        fi
+      fi
     fi
   fi
 fi
